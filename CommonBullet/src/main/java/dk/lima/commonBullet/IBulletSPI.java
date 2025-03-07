c24561ebf1c31d7d463d7d66ca52abe3b5ffbf99
Mikkel Grove <mikkelfgrove@gmail.com>
2025-03-07 11:12:41 +0100

    Weapon-System Added

--- CommonBullet/src/main/java/dk/lima/commonBullet/IBulletSPI.java
+++ CommonBullet/src/main/java/dk/lima/commonBullet/IBulletSPI.java
@@ -1,5 +1,5 @@
 package dk.lima.commonBullet;
 
 public interface IBulletSPI {
-    public Bullet createBullet(double x, double y, double rotation);
+    public Bullet createBullet(double x, double y, double rotation, double radius);
 }
--- Enemy/src/main/java/dk/lima/enemy/EnemyProcessor.java
+++ Enemy/src/main/java/dk/lima/enemy/EnemyProcessor.java
@@ -5,17 +5,25 @@ import dk.lima.common.data.GameData;
 import dk.lima.common.data.World;
 import dk.lima.common.enemy.Enemy;
 import dk.lima.common.services.IEntityProcessingService;
+import dk.lima.common.weapon.IWeaponSPI;
 
+import java.util.Collection;
 import java.util.Random;
+import java.util.ServiceLoader;
+
+import static java.util.stream.Collectors.toList;
 
 public class EnemyProcessor implements IEntityProcessingService {
+
+
     @Override
     public void process(GameData gameData, World world) {
+
+
         Random rnd = new Random();
         for (Entity enemy : world.getEntities(Enemy.class)) {
             double changeX = Math.cos(Math.toRadians(enemy.getRotation()));
             double changeY = Math.sin(Math.toRadians(enemy.getRotation()));
-
 
 
             enemy.setX(enemy.getX() + changeX * 0.5);
@@ -41,6 +49,10 @@ public class EnemyProcessor implements IEntityProcessingService {
             }
             if (rnd.nextInt(90) < 1){
                 enemy.setRotation(15);
+            }
+            if (rnd.nextInt(90) < 2){
+                Enemy e = (Enemy) enemy;
+                e.getIWeaponSPI().shoot(enemy, gameData, world);
             }
         }
     }
