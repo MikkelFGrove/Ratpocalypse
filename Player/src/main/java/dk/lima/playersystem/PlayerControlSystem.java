47d944a99daf44d5640825d6527c31bda78f7c65
MJP <madsjep23@gmail.com>
2025-03-07 10:03:29 +0100

    Added player movement through input

--- Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
+++ Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
@@ -1,14 +1,50 @@
 package dk.lima.playersystem;
 
+import dk.lima.common.data.EGameInputs;
+import dk.lima.common.data.Entity;
 import dk.lima.common.data.GameData;
 import dk.lima.common.data.World;
 import dk.lima.common.services.IEntityProcessingService;
+import dk.lima.commonplayer.Player;
 
 public class PlayerControlSystem implements IEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-        //TODO:
+        for (Entity player: world.getEntities(Player.class)) {
+            double angle = Math.toRadians(player.getRotation());
+            double changeX = Math.cos(angle);
+            double changeY = Math.sin(angle);
 
+            double velocity = 1;
+            double rotationSpeed = 3;
+
+            if (gameData.getInputs().isDown(EGameInputs.UP)) {
+                player.setX(player.getX() + changeX * velocity);
+                player.setY(player.getY() + changeY * velocity);
+            }
+            if (gameData.getInputs().isDown(EGameInputs.DOWN)) {
+                player.setX(player.getX() - changeX * velocity);
+                player.setY(player.getY() - changeY * velocity);
+            }
+            if (gameData.getInputs().isDown(EGameInputs.LEFT)) {
+                player.setRotation(player.getRotation() - rotationSpeed);
+            }
+            if (gameData.getInputs().isDown(EGameInputs.RIGHT)) {
+                player.setRotation(player.getRotation() + rotationSpeed);
+            }
+
+            if (player.getX() < 0) {
+                player.setX(gameData.getDisplayWidth());
+            } else if (player.getX() > gameData.getDisplayWidth()) {
+                player.setX(0);
+            }
+
+            if (player.getY() < 0) {
+                player.setY(gameData.getDisplayHeight());
+            } else if (player.getY() > gameData.getDisplayHeight()) {
+                player.setY(0);
+            }
+        }
     }
 }
