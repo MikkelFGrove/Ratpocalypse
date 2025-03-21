67fcf732b9d05429e8f36a6842ea5a1a9374d2e4
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-03-21 10:02:48 +0100

    Still non-working A* algorithm

--- Pathfinding/src/main/java/dk/lima/pathfinding/AStarPathfinding.java
+++ Pathfinding/src/main/java/dk/lima/pathfinding/AStarPathfinding.java
@@ -3,9 +3,7 @@ package dk.lima.pathfinding;
 import dk.lima.common.data.Coordinate;
 import dk.lima.common.pathfinding.IPathfindingSPI;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AStarPathfinding implements IPathfindingSPI {
     double scalingFactor = 1;
@@ -28,31 +26,39 @@ public class AStarPathfinding implements IPathfindingSPI {
         }
 
         return result;*/
+        Coordinate[] coordinates = calculateNextSteps(start, goal);
+        return coordinates[coordinates.length - 1];
+    }
 
+    @Override
+    public Coordinate[] calculateNextSteps(Coordinate start, Coordinate goal) {
         List<Node> fringe = new ArrayList<>();
         Node initialNode = new Node(start);
         fringe.add(initialNode);
         while (!fringe.isEmpty()) {
             Node currentNode = fringe.getFirst();
             fringe.removeFirst();
-            System.out.println("Goal: " + goal.getX() + " " + goal.getY());
-            System.out.println(currentNode.getCoordinates().getX() + " " + currentNode.getCoordinates().getY());
+            System.out.println(fringe.size());
             if (currentNode.getCoordinates().equals(goal)) {
                 // Return the next steps coordinates.
-                return currentNode.getPath().getLast().getCoordinates();
+                Coordinate[] coordinates = new Coordinate[currentNode.getPath().size()];
+                for (int i = 0; i < coordinates.length; i++) {
+                    coordinates[i] = currentNode.getPath().get(i).getCoordinates();
+                }
+                return coordinates;
             }
-            List<Node> children = List.of(expandNode(currentNode));
+            Node[] children = expandNode(currentNode);
             for (Node child : children) {
                 child.setHeuristicCost(heuristic(child.getCoordinates(), goal));
+                fringe.add(child);
             }
-            fringe.addAll(children);
             fringe.sort(null);
-            /*if (fringe.size() > 5) {
+            if (fringe.size() > 5) {
                 fringe.subList(5, fringe.size()).clear();
-            }*/
+            }
         }
 
-        return start;
+        return new Coordinate[]{start};
     }
 
     private double heuristic(Coordinate start, Coordinate goal) {
@@ -61,16 +67,15 @@ public class AStarPathfinding implements IPathfindingSPI {
     }
 
     private Node[] expandNode(Node parentState){
-        Node[] successorStates = new Node[9];
-        successorStates[0] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()), 0);
-        successorStates[1] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1* scalingFactor, parentState.getCoordinates().getY()), 1);
-        successorStates[2] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() + 1*scalingFactor, parentState.getCoordinates().getY()), 1);
-        successorStates[3] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()+1*scalingFactor), 1);
-        successorStates[4] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()-1*scalingFactor), 1);
-        successorStates[5] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() +1*scalingFactor, parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
-        successorStates[6] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()+1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
-        successorStates[7] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor,parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
-        successorStates[8] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
+        Node[] successorStates = new Node[4];
+        successorStates[0] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1* scalingFactor, parentState.getCoordinates().getY()), 1);
+        successorStates[1] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() + 1*scalingFactor, parentState.getCoordinates().getY()), 1);
+        successorStates[2] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()+1*scalingFactor), 1);
+        successorStates[3] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()-1*scalingFactor), 1);
+        //successorStates[4] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() +1*scalingFactor, parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
+        //successorStates[5] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()+1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
+        //successorStates[6] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor,parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
+        //successorStates[7] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
         return successorStates;
     }
 }
