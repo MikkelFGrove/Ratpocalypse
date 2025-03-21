c9ae3c091c12505a0764b3e201664db036ae58fd
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-03-21 10:02:48 +0100

    Still non-working A* algorithm

--- CommonPathfinding/src/main/java/dk/lima/common/pathfinding/IPathfindingSPI.java
+++ CommonPathfinding/src/main/java/dk/lima/common/pathfinding/IPathfindingSPI.java
@@ -4,4 +4,5 @@ import dk.lima.common.data.Coordinate;
 
 public interface IPathfindingSPI {
     public Coordinate calculateNextStep(Coordinate start, Coordinate goal);
+    public Coordinate[] calculateNextSteps(Coordinate start, Coordinate goal);
 }
