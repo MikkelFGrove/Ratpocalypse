f19d9f235adb6b870103405c041578b36cb6e0ef
Magnus Hende Christensen <magnushende@gmail.com>
2025-03-21 08:42:18 +0100

    comments in playerControl

--- Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
+++ Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
@@ -15,20 +15,39 @@ public class PlayerControlSystem implements IEntityProcessingService {
             player.setRotation(Math.toDegrees(angle));
 
             double velocity = 1.5;
+
+            //Checks what input is registered and then either move, rotate or fires a bullet based on that.
             if (gameData.getInputs().isDown(EGameInputs.UP)) {
+
+                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
                 world.setPlayerY(world.getPlayerY() + velocity);
+
+                //This ensures that as the player moves around the map, the camera gets adjusted to keep them centered.
+                // Other components (collision, weapons, etc.) depends on these coordinates, to work properly.
                 player.setY(-world.getPlayerY() + gameData.getDisplayHeight() / 2);
             }
             if (gameData.getInputs().isDown(EGameInputs.DOWN)) {
+                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
                 world.setPlayerY(world.getPlayerY() - velocity);
+
+                //This ensures that as the player moves around the map, the camera gets adjusted to keep them centered.
+                // Other components (collision, weapons, etc.) depends on these coordinates, to work properly.
                 player.setY(-world.getPlayerY() + gameData.getDisplayHeight() / 2);
             }
             if (gameData.getInputs().isDown(EGameInputs.LEFT)) {
+                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
                 world.setPlayerX(world.getPlayerX() + velocity);
+
+                //This ensures that as the player moves around the map, the camera gets adjusted to keep them centered.
+                // Other components (collision, weapons, etc.) depends on these coordinates, to work properly.
                 player.setX(-world.getPlayerX() + gameData.getDisplayWidth() / 2);
             }
             if (gameData.getInputs().isDown(EGameInputs.RIGHT)) {
+                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
                 world.setPlayerX(world.getPlayerX() - velocity);
+
+                //This ensures that as the player moves around the map, the camera gets adjusted to keep them centered.
+                // Other components (collision, weapons, etc.) depends on these coordinates, to work properly.
                 player.setX(-world.getPlayerX() + gameData.getDisplayWidth() / 2);
             }
             if (gameData.getInputs().isDown(EGameInputs.ACTION)) {
