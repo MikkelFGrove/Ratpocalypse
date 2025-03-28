95c1147bd0162fedd475eeb53b2cbe8a5af78318
MJP <madsjep23@gmail.com>
2025-03-28 08:54:55 +0100

    Fix module-info

--- CommonEntityCP/src/main/java/module-info.java
+++ CommonEntityCP/src/main/java/module-info.java
@@ -1,13 +1,8 @@
-import dk.sdu.cbse.common.bullet.IBulletSPI;
-
 module CommonEntityCP {
     requires Common;
     requires CommonBullet;
-    requires CommonCollision;
 
-    exports dk.sdu.cbse.common.entitycomponents;
-
-    uses IBulletSPI;
+    exports dk.lima.common.entitycomponents;
 }
 
 
