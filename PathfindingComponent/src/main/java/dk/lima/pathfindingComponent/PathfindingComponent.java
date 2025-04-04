80833411c8449260c4b9ea77e8bd89d2eb4447e4
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-04-04 10:36:01 +0200

    Improved A*
    Changed the pathfinding to use dynamic resolution, changing the resolution as it comes closer to the goal

--- PathfindingComponent/src/main/java/dk/lima/pathfindingComponent/PathfindingComponent.java
+++ PathfindingComponent/src/main/java/dk/lima/pathfindingComponent/PathfindingComponent.java
@@ -14,7 +14,7 @@ public class PathfindingComponent implements IEntityComponent {
     private int stepsTaken;
     private Coordinate[] path;
     private Entity entity;
-    private double scalingFactor = 0.5;
+    private double maxScalingFactor = 24;
     // Value specifying how long the player has to move from the calculated path to calculate a new path
     private double goalRadius = 0.65;
 
@@ -30,6 +30,10 @@ public class PathfindingComponent implements IEntityComponent {
         return EntityComponentTypes.PATHFINDING;
     }
 
+    public void setEntity(Entity entity) {
+        this.entity = entity;
+    }
+
     public void process(GameData gameData, World world) {
         if (world.getPlayerPosition() == null){
             return;
@@ -39,55 +43,45 @@ public class PathfindingComponent implements IEntityComponent {
         Coordinate coord = transformCP.getCoord();
         Coordinate nextStep = transformCP.getCoord().clone();
 
-        // If enemy is within the radius, use straight-line pathfinding
-        /*if (heuristic(new Coordinate(entity.getX(), entity.getY()), world.getPlayerPosition()) <= goalRadius) {
-            Coordinate step = calculateStraightlineStep(new Coordinate(entity.getX(), entity.getY()), world.getPlayerPosition());
-            nextStep.setX(step.getX());
-            nextStep.setY(step.getY());
-        } else {*/
-            // Use A* for pathfinding if enemy is outside radius
-            // Calculate new path if not existing, or if player has moved outside radius
-            if (path == null || heuristic(path[path.length - 1], world.getPlayerPosition()) > goalRadius * heuristic(coord, world.getPlayerPosition())) {
-                path = calculatePath(coord, world.getPlayerPosition());
-                stepsTaken = 0;
-            }
+        // Calculate new path if path is empty or if player has moved a certain distance from current goal
+        if (path == null || heuristic(path[path.length - 1], world.getPlayerPosition()) > goalRadius * heuristic(coord, world.getPlayerPosition())) {
+            path = calculatePath(coord, world.getPlayerPosition());
+            stepsTaken = 0;
+        }
 
-            if (path.length > stepsTaken) {
-                nextStep = path[stepsTaken];
-                // Increase amount of steps taken in the current A* path.
-                stepsTaken++;
-            }
-        //}
+        if (path.length > stepsTaken) {
+            nextStep = path[stepsTaken];
+        }
+
+        Coordinate calculatedStep = calculateStraightLineStep(coord, nextStep, 1);
 
         // We use the coordinates of the player to angle the enemy
         double yDiff = world.getPlayerPosition().getY() - coord.getY();
         double xDiff = world.getPlayerPosition().getX() - coord.getX();
+        //double yDiff = calculatedStep.getY() - coord.getY();
+        //double xDiff = calculatedStep.getX() - coord.getX();
         double angle = Math.toDegrees(Math.atan2(yDiff, xDiff));
 
-        transformCP.setCoord(nextStep);
+        if (calculatedStep.equals(nextStep)) {
+            stepsTaken++;
+        }
+
+        transformCP.setCoord(calculatedStep);
         transformCP.setRotation(angle);
     }
 
-    private Coordinate calculateStraightlineStep(Coordinate start, Coordinate goal) {
-        Coordinate result = new Coordinate(start.getX(), start.getY());
-        int yDiff = (int) (goal.getY() - start.getY());
-        int xDiff = (int) (goal.getX() - start.getX());
-
-        TransformCP transformCP = (TransformCP) entity.getComponent(EntityComponentTypes.TRANSFORM);
-        Coordinate coord = transformCP.getCoord();
-
-        if (yDiff > 0) {
-            result.setY(coord.getY() + scalingFactor);
-        } else if (yDiff < 0) {
-            result.setY(coord.getY() - scalingFactor);
+    private Coordinate calculateStraightLineStep(Coordinate start, Coordinate goal, double length) {
+        if (start.equals(goal)) {
+            return start;
         }
 
-        if (xDiff > 0) {
-            result.setX(coord.getX() + scalingFactor);
-        } else if (xDiff < 0) {
-            result.setX(coord.getX() - scalingFactor);
-        }
-        return result;
+        double xDistance = goal.getX() - start.getX();
+        double yDistance = goal.getY() - start.getY();
+
+        double hyp = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        double scaledHyp = hyp / length;
+
+        return new Coordinate(start.getX() + xDistance / scaledHyp, start.getY() + yDistance / scaledHyp);
     }
 
     public Coordinate[] calculatePath(Coordinate start, Coordinate goal) {
@@ -111,7 +105,16 @@ public class PathfindingComponent implements IEntityComponent {
                 }
                 return coordinates;
             }
-            Node[] children = expandNode(currentNode);
+
+            Node[] children;
+
+            double dist = heuristic(goal, currentNode.getCoordinates());
+            if (dist >= maxScalingFactor) {
+                children = expandNode(currentNode, maxScalingFactor);
+            } else {
+                children = expandNode(currentNode, dist);
+            }
+
             for (Node child : children) {
                 child.setHeuristicCost(heuristic(child.getCoordinates(), goal));
                 fringe.add(child);
@@ -130,20 +133,16 @@ public class PathfindingComponent implements IEntityComponent {
         return Math.sqrt(Math.pow((goal.getX() - start.getX()), 2) + Math.pow((goal.getY() - start.getY()), 2));
     }
 
-    private Node[] expandNode(Node parentState){
-        Node[] successorStates = new Node[8];
+    private Node[] expandNode(Node parentState, double scalingFactor){
+        Node[] successorStates = new Node[4];
         successorStates[0] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1* scalingFactor, parentState.getCoordinates().getY()), 1);
         successorStates[1] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() + 1*scalingFactor, parentState.getCoordinates().getY()), 1);
         successorStates[2] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()+1*scalingFactor), 1);
         successorStates[3] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX(), parentState.getCoordinates().getY()-1*scalingFactor), 1);
-        successorStates[4] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() +1*scalingFactor, parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
-        successorStates[5] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()+1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
-        successorStates[6] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor,parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
-        successorStates[7] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
+        //successorStates[4] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX() +1*scalingFactor, parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
+        //successorStates[5] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()+1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
+        //successorStates[6] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor,parentState.getCoordinates().getY()+1*scalingFactor), Math.sqrt(2));
+        //successorStates[7] = new Node(parentState, new Coordinate(parentState.getCoordinates().getX()-1*scalingFactor, parentState.getCoordinates().getY()-1*scalingFactor), Math.sqrt(2));
         return successorStates;
-    }
-
-    public void setEntity(Entity entity) {
-        this.entity = entity;
     }
 }
