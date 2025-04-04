0dda03c1990a14c59688f18e84645f232aee490d
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-04-04 09:20:27 +0200

    Added Unit Tests for scoring in collisionDetector
    Added test for collision with bullet and player

--- Collision/src/test/java/dk/lima/collisionSystem/CollisionDetectorTest.java
+++ Collision/src/test/java/dk/lima/collisionSystem/CollisionDetectorTest.java
@@ -2,6 +2,7 @@ package dk.lima.collisionSystem;
 
 import dk.lima.common.bullet.Bullet;
 import dk.lima.common.data.Coordinate;
+import dk.lima.common.data.EEntityTypes;
 import dk.lima.common.entity.Entity;
 import dk.lima.common.data.GameData;
 import dk.lima.common.data.World;
@@ -31,6 +32,11 @@ class CollisionDetectorTest {
         }
     }
 
+    @AfterEach
+    public void resetScore() {
+        gameData.setScore(0);
+    }
+
     @Test
     public void testBulletEntityCollisionRemovesBoth() {
         Bullet bullet = new Bullet();
@@ -58,5 +64,61 @@ class CollisionDetectorTest {
         // Check that both the bullet and entity is removed
         assertFalse(world.getEntities().contains(entity));
         assertFalse(world.getEntities().contains(bullet));
+    }
+
+    @Test
+    public void testKillsIncreaseScore() {
+        Bullet bullet = new Bullet();
+        bullet.addComponent(new TransformCP(
+                new Coordinate(0,0),
+                0,
+                1
+        ));
+        bullet.setEntityType(EEntityTypes.BULLET);
+
+        Entity entity = new Entity();
+        entity.addComponent(new TransformCP(
+                new Coordinate(0,0),
+                0,
+                1
+        ));
+        entity.setEntityType(EEntityTypes.ENEMY);
+
+        world.addEntity(bullet);
+        world.addEntity(entity);
+
+        assertEquals(0, gameData.getScore());
+
+        collisionDetector.process(gameData, world);
+
+        assertEquals(1, gameData.getScore());
+    }
+
+    @Test
+    public void testCollisionDoesNotIncreasesScore() {
+        Entity player = new Entity();
+        player.addComponent(new TransformCP(
+                new Coordinate(0,0),
+                0,
+                1
+        ));
+        player.setEntityType(EEntityTypes.PLAYER);
+
+        Entity enemy = new Entity();
+        enemy.addComponent(new TransformCP(
+                new Coordinate(0,0),
+                0,
+                1
+        ));
+        enemy.setEntityType(EEntityTypes.ENEMY);
+
+        world.addEntity(player);
+        world.addEntity(enemy);
+
+        assertEquals(0, gameData.getScore());
+
+        collisionDetector.process(gameData, world);
+
+        assertEquals(0, gameData.getScore());
     }
 }
