71e29713b490d7f1810ae0afca799c6ad771d0a9
Andreas528 <andreas.runge528@gmail.com>
2025-04-11 17:33:48 +0200

    Fixed Back2Menu buttons
    Fixed issue where it would not reset the game

--- Graphics/src/main/java/dk/lima/graphics/menuRender/PauseMenu.java
+++ Graphics/src/main/java/dk/lima/graphics/menuRender/PauseMenu.java
@@ -153,6 +153,7 @@ public class PauseMenu implements IMenu {
 
             gameData.setScore(0);
             gameData.setDuration(Duration.ZERO);
+            gameData.setCurrentWave(0);
             world.getEntities().clear();
             world.setPlayerPosition(new Coordinate(0, 0));
 
