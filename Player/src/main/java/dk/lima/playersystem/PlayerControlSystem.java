d7bfec85c6c38e04ec4b9936723c22e1b9717d60
Magnus Hende Christensen <magnushende@gmail.com>
2025-05-09 09:35:10 +0200

    Moved player Movement into its own component

--- Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
+++ Player/src/main/java/dk/lima/playersystem/PlayerControlSystem.java
@@ -13,36 +13,12 @@ public class PlayerControlSystem implements IEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-        for (Entity player: world.getEntities(Player.class)) {
-            TransformCP transformCP = (TransformCP) player.getComponent(EntityComponentTypes.TRANSFORM);
+
+        for(Entity player : world.getEntities(Player.class)) {
+
             WeaponCP weaponCP = (WeaponCP) player.getComponent(EntityComponentTypes.WEAPON);
 
-            double x = gameData.getMousePosition().getX() - gameData.getDisplayWidth() / 2d;
-            double y = gameData.getMousePosition().getY() - gameData.getDisplayHeight() / 2d;
-            double angle = Math.atan2(y, x);
-            transformCP.setRotation(Math.toDegrees(angle));
-
-            Coordinate playerCoord = transformCP.getCoord();
-            double velocity = 2.5;
-            //Checks what input is registered and then either move, rotate or fires a bullet based on that.
-            if (gameData.getInputs().isDown(EGameInputs.UP)) {
-                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
-                playerCoord.setY(playerCoord.getY() - velocity);
-            }
-            if (gameData.getInputs().isDown(EGameInputs.DOWN)) {
-                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
-                playerCoord.setY(playerCoord.getY() + velocity);
-            }
-            if (gameData.getInputs().isDown(EGameInputs.LEFT)) {
-                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
-                playerCoord.setX(playerCoord.getX() - velocity);
-            }
-            if (gameData.getInputs().isDown(EGameInputs.RIGHT)) {
-                //Updates the player's world position ensuring it can move, and keeps track on where the player is in the world.
-                playerCoord.setX(playerCoord.getX() + velocity);
-            }
-            world.setPlayerPosition(playerCoord);
-            
+            weaponCP.setShouldAttack(gameData.getInputs().isDown(EGameInputs.ACTION));
             weaponCP.process(gameData, world);
 
             HealthCP healthCP = (HealthCP) player.getComponent(EntityComponentTypes.HEALTH);
