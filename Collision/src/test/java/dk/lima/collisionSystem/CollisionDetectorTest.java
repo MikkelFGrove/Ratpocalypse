a2b2bfb7b476a855cebbb1a024c96c7fd159b2c6
cgrevy <chgre23@student.sdu.dk>
2025-05-16 12:01:42 +0200

    Player and CollisionTest

--- Collision/src/test/java/dk/lima/collisionSystem/CollisionDetectorTest.java
+++ Collision/src/test/java/dk/lima/collisionSystem/CollisionDetectorTest.java
@@ -1,5 +1,6 @@
 package dk.lima.collisionSystem;
 
+import dk.lima.bullet.BulletGamePlugin;
 import dk.lima.common.bullet.Bullet;
 import dk.lima.common.data.Coordinate;
 import dk.lima.common.data.EEntityTypes;
@@ -7,7 +8,12 @@ import dk.lima.common.entity.Entity;
 import dk.lima.common.data.GameData;
 import dk.lima.common.data.World;
 import dk.lima.common.entity.EntityComponentTypes;
+import dk.lima.common.entitycomponents.DamageCP;
+import dk.lima.common.entitycomponents.HealthCP;
 import dk.lima.common.entitycomponents.TransformCP;
+import dk.lima.common.player.Player;
+import dk.lima.playersystem.PlayerCollisionHandler;
+import dk.lima.playersystem.PlayerPlugin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -37,25 +43,19 @@ class CollisionDetectorTest {
     public void resetScore() {
         gameData.setScore(0);
     }
-/*
+
     @Test
-    public void testBulletEntityCollisionRemovesBoth() {
-        Bullet bullet = new Bullet();
-        bullet.setEntityType(EEntityTypes.BULLET);
-        bullet.addComponent(new CollisionCP(bullet));
-        bullet.addComponent(new TransformCP(
-                new Coordinate(0,0),
-                0,
-                1
-        ));
+    public void testBulletCollisionRemovesBullet() {
+        BulletGamePlugin bulletGamePlugin = new BulletGamePlugin();
+        Bullet bullet = bulletGamePlugin.createBullet(0,0,0,1,1);
 
         Entity entity = new Entity();
-        entity.addComponent(new CollisionCP(entity));
-        entity.addComponent(new TransformCP(
-                new Coordinate(0,0),
-                0,
-                1
-        ));
+        //bullet's coordinates are changed in create bullet. The coordinates below reflect that change
+
+        entity.addComponent(new TransformCP(new Coordinate(2.5,0),0,1));
+        DamageCP dmCP = new DamageCP();
+        dmCP.setAttackDamage(2);
+        entity.addComponent(dmCP);
 
         world.addEntity(bullet);
         world.addEntity(entity);
@@ -64,13 +64,37 @@ class CollisionDetectorTest {
         assertTrue(world.getEntities().contains(bullet));
 
         collisionDetector.process(gameData, world);
-        bullet.getComponent(EntityComponentTypes.COLLISION).process(gameData, world);
 
-        // Check that both the bullet and entity is removed
-        assertFalse(world.getEntities().contains(entity));
+        // Check that the bullet is removed
         assertFalse(world.getEntities().contains(bullet));
     }
 
+    @Test
+    public void testCollisionDecreasesHealth(){
+        BulletGamePlugin bulletGamePlugin = new BulletGamePlugin();
+        Bullet bullet = bulletGamePlugin.createBullet(0,0,0,1,1);
+
+        Entity entity = new Entity();
+        entity.addComponent(new HealthCP());
+        HealthCP healthCP= (HealthCP) entity.getComponent(EntityComponentTypes.HEALTH);
+        healthCP.setHealth(10);
+        entity.addComponent(new PlayerCollisionHandler());
+        entity.getComponent(EntityComponentTypes.COLLISION).setEntity(entity);
+        assertEquals(10, healthCP.getHealth());
+
+        //bullet's coordinates are changed in create bullet. The coordinates below reflect that change
+        entity.addComponent(new TransformCP(new Coordinate(2.5,0),0,1));
+
+        healthCP = (HealthCP) entity.getComponent(EntityComponentTypes.HEALTH);
+
+        world.addEntity(bullet);
+        world.addEntity(entity);
+        collisionDetector.process(gameData, world);
+
+        assertEquals(9, healthCP.getHealth());
+
+    }
+/*
     @Test
     public void testKillsIncreaseScore() {
         Bullet bullet = new Bullet();
@@ -132,5 +156,5 @@ class CollisionDetectorTest {
         player.getComponent(EntityComponentTypes.COLLISION).process(gameData, world);
 
         assertEquals(0, gameData.getScore());
-    }*/
+    } */
 }
